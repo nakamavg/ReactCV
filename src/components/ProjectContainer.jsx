dd55f0d4c3234fa231a8df81027262d54d40b0dd
Nakamavg <aecm.davidgomez@gmail.com>
2025-02-07 08:44:11 +0100

    fixed tablets

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -21,7 +21,7 @@ const ProjectsContainer = () => {
     return (
         <div className="container mx-auto p-8">
             <h2 className="text-3xl font-bold mb-6 text-center text-yellow-400">Proyectos</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 gap-8">
                 {projects.map((project, index) => (
                     <ProjectCard
                         key={index}
